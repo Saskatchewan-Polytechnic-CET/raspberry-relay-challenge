05d653b372d300493897b964c60c989c8db666af
Joshua Newell <newellj@saskpolytech.ca>
2025-11-13 14:14:09 -0600

    Update setup.bash
    Need to run code once so it appears on the menu.

--- setup.bash
+++ setup.bash
@@ -5,3 +5,4 @@ sudo apt install -y code gpg
 code --install-extension ms-python.python
 code --install-extension jvalecillos.gpg
 code --install-extension tomoki1207.pdf
+code ~/Desktop/cet-exercise&
